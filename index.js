52f41c2584eba7f2228d255e2a8ad62a9970f9a8
theritikkk <ritikraj2004@gmail.com>
2025-08-10 20:02:41 +0530

    feat: implement clustering for improved performance and refactor chat message handling

--- index.js
+++ index.js
@@ -5,69 +5,125 @@ import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
-// open the database file
-const db = await open({
-  filename: 'chat.db',
-  driver: sqlite3.Database
-});
+if( cluster.isPrimary ) {
 
-// create our 'messages' table (you can ignore the 'client_offset' column for now)
-await db.exec(`
-  CREATE TABLE IF NOT EXISTS messages (
+  const numCPUs = availableParallelism();
+
+  for( let i = 0; i < numCPUs; i++ ) {
+
+    cluster.fork(
+        {
+            PORT: 3000 + i
+        }
+    );
+  }
+
+  setupPrimary();
+
+} 
+else {
+
+  const db = await open(
+        {
+            filename: 'chat.db',
+            driver: sqlite3.Database
+        }
+    );
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       client_offset TEXT UNIQUE,
       content TEXT
-  );
-`);
+    );
+  `);
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+  const app = express();
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+  const server = createServer( app );
 
-app.get( '/', ( req, res ) => {
+  const io = new Server( server, {
 
-  res.sendFile( join( __dirname, 'index.html' ) );
+    connectionStateRecovery: {},
 
-});
-
-io.on('connection', async (socket) => {
-  socket.on('chat message', async (msg, clientOffset, callback) => {
-    let result;
-    try {
-      result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
-    } catch (e) {
-      if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
-        // the message was already inserted, so we notify the client
-        callback();
-      } else {
-        // nothing to do, just let the client retry
-      }
-      return;
-    }
-    io.emit('chat message', msg, result.lastID);
-    // acknowledge the event
-    callback();
+    adapter: createAdapter()
+    
   });
 
-  if (!socket.recovered) {
-    try {
-      await db.each('SELECT id, content FROM messages WHERE id > ?',
-        [socket.handshake.auth.serverOffset || 0],
-        (_err, row) => {
-          socket.emit('chat message', row.content, row.id);
+  const __dirname = dirname( fileURLToPath( import.meta.url ) );
+
+  app.get( '/', ( req, res ) => {
+    
+    res.sendFile( join ( __dirname, 'index.html' ) );
+
+  });
+
+
+  io.on( 'connection', async ( socket ) => {
+    
+    socket.on( 'chat message', async ( msg, clientOffset, callback ) => {
+      
+        let result;
+
+      try {
+        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
+
+      } catch( e ) {
+
+        if( e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+
+          callback();
+
+        } else {
+          
+            // nothing to do, just let the client retry
+
         }
-      )
-    } catch (e) {
-      // something went wrong
+
+        return;
+      }
+
+      io.emit( 'chat message', msg, result.lastID );
+
+      callback();
+
+    });
+
+    if( !socket.recovered ) {
+
+      try {
+
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+
+            socket.emit('chat message', row.content, row.id);
+          }
+
+        )
+
+      } catch (e) {
+
+        // something went wrong
+
+      }
+
     }
-  }
-});
 
-server.listen( 3000, () => {
+  });
 
-  console.log( ' Server running at http://localhost:3000 . ' );
 
-});
+  const port = process.env.PORT;
+
+
+  server.listen( port, () => {
+
+    console.log( ` Server running at http://localhost:${port} .` );
+
+  });
+  
+}
